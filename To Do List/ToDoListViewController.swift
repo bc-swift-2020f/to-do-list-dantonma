cbc8e837551e7825d070d25274bfe84852f6a509
Marissa D'Antonio <dantonma@bc.edu>
2020-09-26 18:10:27 -0400

    Completed -> 5.2

--- To Do List/ToDoListViewController.swift
+++ To Do List/ToDoListViewController.swift
@@ -24,5 +24,14 @@ class ToDoListViewController: UIViewController {
 }
 
 extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
+    }
     
 }
